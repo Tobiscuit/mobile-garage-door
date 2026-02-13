e046cc1286de6c7f0bcc3f94b17f18bb574a340e
Tobi <33387497+Tobiscuit@users.noreply.github.com>
2026-02-13 16:59:23 -0600

    fix: remove broken migration import

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,2 @@
-import * as migration_20260213_224320 from './20260213_224320';
-
 export const migrations = [
-  {
-    up: migration_20260213_224320.up,
-    down: migration_20260213_224320.down,
-    name: '20260213_224320'
-  },
 ];
