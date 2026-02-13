9ff5d8d462aa4ffe49092d93bdbc0c40cb490bce
Tobi <33387497+Tobiscuit@users.noreply.github.com>
2026-02-13 17:08:55 -0600

    fix: add migration for users table schema updates

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,2 +1,9 @@
+import * as migration_20260213_230840 from './20260213_230840';
+
 export const migrations = [
+  {
+    up: migration_20260213_230840.up,
+    down: migration_20260213_230840.down,
+    name: '20260213_230840'
+  },
 ];
